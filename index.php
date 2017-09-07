d4ee38b1d3a8288195a31a6ae2236247721cb1ee
Ana Pena <anpena@csumb.edu>
2017-09-07 18:04:20 +0000

    Hw1

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+       <meta charset=utf-8"/>
+     <title> Python </title>
+     
+     <link href="style.css" rel="stylesheet" type="text/css" />
+     
+  
+    </head>
+    
+    <body>
+        <header>
+            <h1 class="Py"> Python </h1>
+            <h2> By: Ana Pena </h2>
+        <header/>
+        
+        
+        
+    <nav>
+        <hr width= "50%" />
+        <a href="index.php">Overview</a>
+        <a href="history.html">History</a>
+        <a href="library.html">Libraries</a>
+        <a href="sources.html">Sources</a>
+    </nav>
+    
+    <br /> <br/>
+    
+    
+    <main>
+    <img src="images/python.png" alt="Picture of Ana Pena" />
+    
+    
+    <div class="Letters">Python is a powerfull programming Language. It is very Easy to learn  and be able to use it anywhere. Python is an open source which makes it freedly usable and distributable.
+    </div>
+    </main>
+    
+     <footer>
+     
+             <hr>
+             CST 336. 2017&copy; Pena <br />
+             <strong>Disclaimer:</strong> The information in this webpage is fictitous. <br />
+             Is is used for academic purposes only.
+            
+    </footer>  
+    
+ </body>
+ 
+    
+</html>
